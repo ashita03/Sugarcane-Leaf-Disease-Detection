3fe8b8d8f047f37484b855092be98ea3ac7e492f
ashita03 <ashitashetty1@gmail.com>
2024-04-04 18:38:10 -0400

    Updated Streamlit App

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ def import_and_predict(image_data, model):
 if file is None:
     st.text("Please upload an image file")
 else:
+    model = tf.keras.models.load_model('weights\efficientnet_model_sg.h5')
     image = Image.open(file)
     st.image(image, use_column_width=True)
     predictions = import_and_predict(image, model)
